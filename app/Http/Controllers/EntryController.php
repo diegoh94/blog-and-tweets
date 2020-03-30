ecd5b51f8e7e3ebb1b10782b6a8f705dcd523636
Diego Huacanjulca <control@seo-arquitectos.com>
2020-03-30 00:37:12 -0500

    create form new entry

--- app/Http/Controllers/EntryController.php
+++ app/Http/Controllers/EntryController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Entry;
 use Illuminate\Http\Request;
 
 class EntryController extends Controller
 {
-    //
+    public function create() {
+        return view('entries.create');
+    }
+    public function store(Request $request) {
+        //dd($request->all()) ;
+        $validatedData=$request->validate([
+        'title' => 'required|min:7|max:255|unique:entries',
+        'content' => 'required|min:25|max:3000'
+        ]);
+
+        $entry = new Entry();
+        $entry->title = $validatedData['title'];
+        $entry->content = $validatedData['content'];
+        $entry->user_id = auth()->id();
+        $entry->save();//insert
+
+        $status = 'Your entry has been published successfully';
+        return back()->with(compact('status'));
+    }
 }
